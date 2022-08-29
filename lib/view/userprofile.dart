95dc8241c5f9903d4f638f30cb9cb911918b62b5
asmaaelshabaka <asmaaelshabaka@yahoo.com>
2022-08-29 23:37:39 +0200

    User Profile Screen

--- lib/view/userprofile.dart
+++ lib/view/userprofile.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class UserProfile extends StatelessWidget {
@@ -52,17 +54,148 @@ class UserProfile extends StatelessWidget {
               Positioned(
                   top: 30,
                   left: (MediaQuery.of(context).size.width / 2) - 100,
-                  child: Hero(
-                    tag: 'ProfileImage',
-                    child: Container(
-                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(100),
-                        image: DecorationImage(
-                            image: AssetImage('images/ProfileImage.jpg'),
-                            fit: BoxFit.cover),
+                  child: Column(
+                    children: [
+                      Hero(
+                        tag: 'ProfileImage',
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            image: DecorationImage(
+                                image: AssetImage('images/ProfileImage.jpg'),
+                                fit: BoxFit.cover),
+                          ),
+                          height: 200,
+                          width: 200,
+                        ),
                       ),
-                      height: 200,
-                      width: 200,
-                    ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  )),
+              Positioned(
+                  top: 250,
+                  left: 25,
+                  right: 25,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Your Score is 50 points',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Achieved Challenges',
+                                style:
+                                    TextStyle(fontSize: 20, color: Colors.grey),
+                              ),
+                              Container(
+                                height: 25,
+                                color: Colors.grey,
+                                width: 1,
+                              ),
+                              Container(
+                                width: 125,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(17.0),
+                                    color: Color(0xFF7A9BEE)),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    InkWell(
+                                      onTap: () {},
+                                      child: Container(
+                                        height: 25,
+                                        width: 25,
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(7),
+                                            color: Color(0xFF7A9BEE)),
+                                        child: Text(
+                                          '20 ',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18),
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Unachieved Challenges',
+                                style:
+                                    TextStyle(fontSize: 20, color: Colors.grey),
+                              ),
+                              Container(
+                                height: 25,
+                                color: Colors.grey,
+                                width: 1,
+                              ),
+                              Container(
+                                width: 125,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(17.0),
+                                    color: Colors.redAccent),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    InkWell(
+                                      onTap: () {},
+                                      child: Container(
+                                        height: 25,
+                                        width: 25,
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(7),
+                                            color: Colors.redAccent),
+                                        child: Text(
+                                          '18 ',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18),
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              ListView.builder(
+                                  itemCount: 12,
+                                  itemBuilder: (context, index) {
+                                    return Container(child: Column(children: [],),);
+                                  })
+                            ],
+                          )
+                        ],
+                      )
+                    ],
                   ))
             ],
           )
